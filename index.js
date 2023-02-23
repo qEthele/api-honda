d5909166ac18cccfffc5e31a3137b16151ea3dc6
Thawatchai <yua.mch@gmail.com>
2023-02-23 18:13:17 +0700

    api7

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use(cors());
 
 let luckyPlayerData = [];
 
