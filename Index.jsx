a8c8d43acbdfde0c850fc3d5c58bec2a36c6e65f
Thawatchai <yua.mch@gmail.com>
2023-02-23 16:20:18 +0700

    Honda api 1

---
+++ Index.jsx
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+let luckyPlayerData = [];
+
+app.get("/", (req, res) => {
+  res.sendFile("/home.html", { root: __dirname });
+});
+
+app.get("/data", (req, res) => {
+  res.send(luckyPlayerData);
+});
+app.get("/showData", (req, res) => {
+  res.sendFile("/showData.html", { root: __dirname });
+});
+
+app.get("/resetAllData", (req, res) => {
+  luckyPlayerData = [];
+  res.send("Reset All Data !!!");
+});
+
+app.post("/mydata", (req, res) => {
+  console.log(luckyPlayerData.length);
+  if (req.body.status === 1) {
+    if (luckyPlayerData.length < 10) {
+      const data = { name: req.body.name, status: req.body.status };
+      luckyPlayerData = luckyPlayerData.concat(data);
+      res.send(data);
+    } else {
+      const data = { name: req.body.name, status: 0 };
+      res.send(data);
+    }
+  } else {
+    const data = { name: req.body.name, status: req.body.status };
+    res.send(data);
+  }
+});
+
+app.listen({ port }, () => {
+  console.log(`Server is running on port ${port}`);
+});
