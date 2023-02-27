b1d788c606388db8f722e25acb245c84b3f663aa
Thawatchai <yua.mch@gmail.com>
2023-02-27 11:43:27 +0700

    add environment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -50,6 +50,6 @@ app.post("/mydata", (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || { port }, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
